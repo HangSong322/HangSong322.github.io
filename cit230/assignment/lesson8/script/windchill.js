7d94313857a2b5ea666e8c2072e9c6bf822b1e24
HangSong322 <38730447+HangSong322@users.noreply.github.com>
2018-06-23 00:04:16 -0600

    lesson9

--- cit230/assignment/lesson8/script/windchill.js
+++ cit230/assignment/lesson8/script/windchill.js
@@ -1,7 +1,5 @@
-var high = parseFloat(document.getElementById('highTem').value);
-var low = parseFloat(document.getElementById('lowTem').value);
-var t = (high + low) / 2;
-var s = parseFloat(document.getElementById('windSpeed').value);
+var t = (90 + 66) / 2;
+var s = 5;
 var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t  * Math.pow(s, 0.16);
 
 document.getElementById('windchill').innerHTML = "Wind Chill:  " + Math.round(f) + "&deg; F";
