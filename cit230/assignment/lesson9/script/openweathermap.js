cab866fcb876e6e83d418e45e422be1803dccdd3
HangSong322 <38730447+HangSong322@users.noreply.github.com>
2018-06-22 23:50:04 -0600

    lesson9assignment

---
+++ cit230/assignment/lesson9/script/openweathermap.js
@@ -0,0 +1,22 @@
+var weatherObj = new XMLHttpRequest();
+
+weatherObj.open('GET','http://api.openweathermap.org/data/2.5/weather?zip=55333,us&appid=b93e92055f66c2277db075668d74c47a&units=imperial', true)
+//weatherObj.responseType = 'json';
+weatherObj.send();
+
+weatherObj.onload = function() {
+    var weatherInfo = JSON.parse(weatherObj.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById('highTem').innerHTML = weatherInfo.main.temp_max;
+    document.getElementById('lowTem').innerHTML = weatherInfo.main.temp_min;
+    document.getElementById('cTem').innerHTML = weatherInfo.main.temp;
+    document.getElementById('weatherDesc').innerHTML = weatherInfo.weather["0"].description.charAt(0).toUpperCase() + weatherInfo.weather["0"].description.slice(1);
+    var weatherIconCode = weatherInfo.weather["0"].icon;
+    var iconPath = "http://openweathermap.org/img/w/" + weatherIconCode + ".png";
+    document.getElementById('weatherIcon').src = iconPath;
+    document.getElementById('humidity').innerHTML = weatherInfo.main.humidity;
+    document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
+//    var weatherData = request.response;
+//    showData(weatherData);
+}
