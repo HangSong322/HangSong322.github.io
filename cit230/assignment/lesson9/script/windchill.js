3f65096781f10483dc4c61e5edea0cb4dfdefb56
HangSong322 <38730447+HangSong322@users.noreply.github.com>
2018-06-23 19:53:00 -0600

    lesson9

--- cit230/assignment/lesson9/script/windchill.js
+++ cit230/assignment/lesson9/script/windchill.js
@@ -1,7 +1,19 @@
-var high = parseFloat(document.getElementById('highTem').value);
-var low = parseFloat(document.getElementById('lowTem').value);
-var t = (high + low) / 2;
-var s = parseFloat(document.getElementById('windSpeed').value);
-var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t  * Math.pow(s, 0.16);
+var weatherObj = new XMLHttpRequest();
 
-document.getElementById('windchill').innerHTML = "Wind Chill:  " + Math.round(f) + "&deg; F";
+weatherObj.open('GET','http://api.openweathermap.org/data/2.5/weather?zip=55333,us&appid=b93e92055f66c2277db075668d74c47a&units=imperial', true)
+//weatherObj.responseType = 'json';
+weatherObj.send();
+
+weatherObj.onload = function() {
+    var weatherInfo = JSON.parse(weatherObj.responseText);
+//    var weatherInfo = weatherObj.response;
+    console.log(weatherInfo);
+    
+    var high = weatherInfo.main.temp_max;
+    var low = weatherInfo.main.temp_min;
+    var t = (high + low) / 2;
+    var s = weatherInfo.wind.speed;
+    var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t  * Math.pow(s, 0.16);
+
+    document.getElementById('windchill').innerHTML = Math.round(f);
+}
