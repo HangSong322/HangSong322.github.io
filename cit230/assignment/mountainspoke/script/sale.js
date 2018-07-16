004c8f68c209d738e4147a53bd0a48fde5783872
HangSong322 <38730447+HangSong322@users.noreply.github.com>
2018-07-15 19:54:55 -0600

    lesson12

---
+++ cit230/assignment/mountainspoke/script/sale.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#mountain-bike-btn").click(function () {
+        $(".mountain-bike-info").toggle();
+    });
+    $("#road-bike-btn").click(function () {
+        $(".road-bike-info").toggle();
+    });
+    $("#hybrid-bike-btn").click(function () {
+        $(".hybrid-bike-info").toggle();
+    });
+});
